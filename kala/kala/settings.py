ff5166ce399f79b8e0cce695de9904ff433b1f56
stark3693 <karanpatel2832005@gmail.com>
2023-07-17 14:37:37 +0530

    news

--- kala/kala/settings.py
+++ kala/kala/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kalu',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'cipherinstitutebpl@gmail.com'
+EMAIL_HOST_PASSWORD = 'ecbvigvjdbmiktsl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- kala/kalu/views.py
+++ kala/kalu/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import BadHeaderError, send_mail
 def home(request):
-    return render(request,'index.html')
+    if request.method=="POST":
+        name=request.POST['name']
+        message=request.POST['message']
+        Email=request.POST['Email']
+        send_mail(
+            'Contact Form',
+            message,
+            'settings.EMAIL_HOST_USER',
+            [Email],
+            fail_silently=False
+        )
+    return render(request,'index.html')     
+        
+      
+        
+         
+       
+            
+       
+    		
+
+		   
+       
+        
+    		
+        
+   
 
 # Create your views here.
